1415e879cda8451bd11dd69f147668bef29ccbdb
David Sabalete <dsabalete@gmail.com>
2019-10-09 09:55:36 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BreedList from './breed-list/BreedList'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <BreedList />
+        </div>
+        <div className="col-sm-6">
+          Another thing
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/breed-list/BreedList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BreedList extends Component {
+    render() {
+        return (
+            <div>
+                BreadList
+            </div>
+        )
+    }
+}
